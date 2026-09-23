c6823483ebd26ecde0ee4389a8ec69e7b45a9178
davidkysilka238-afk <davidkysilka238@gmail.com>
2026-09-23 16:15:29 +0200

    Create basic frontend

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import './style.css';
+
+document.querySelector('#app').innerHTML = `
+  <section class="hero" aria-labelledby="page-title">
+    <div class="hero__glow" aria-hidden="true"></div>
+    <h1 id="page-title">Think different Academy</h1>
+  </section>
+`;
